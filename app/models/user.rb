b342c27204be2b6a815e4a5373abd427a9cec9c0
hodak <kaczor555@gmail.com>
2015-06-04 12:23:19 +0200

    Order User records by created_at by default
    Because of using uuids

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   has_many :exams, through: :user_exams
   has_many :user_answers
   has_many :answers, through: :user_answers
+
+  default_scope { order('created_at ASC') }
 end
